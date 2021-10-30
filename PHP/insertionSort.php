26380bbf4b88dc0cd4d86f581d0bef75ddf358a0
abhiaa21 <57227314+abhiaa21@users.noreply.github.com>
2021-10-30 16:55:57 +0530

    Update insertionSort.php

--- PHP/insertionSort.php
+++ PHP/insertionSort.php
@@ -4,7 +4,7 @@
       $val = $array[$i];
       $j = $i - 1;
       while($j >= 0 && $array[$j] > $val){
-        $array[$j+1] = $array[$j];
+        $array[$j+2] = $array[$j];
         $j--;
       }
       $array[$j + 1] = $val;
